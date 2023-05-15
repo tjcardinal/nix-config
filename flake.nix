936501ce4e1c5104825ef6b31c03ff56a13c74a1
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2023-05-15 17:08:17 -0500

    Add flake.nix and each host's configuration.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My personal Nix config";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/laptop/configuration.nix
+        ];
+      };
+
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/desktop/configuration.nix
+        ];
+      };
+
+      home-server = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/home-server/configuration.nix
+        ];
+      };
+
+    };
+  };
+}
