f99ea2c170e5221096b331d636df8b2e63bb04f0
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2023-05-20 23:00:06 -0500

    Fix missing ;

--- modules/browser.nix
+++ modules/browser.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ firefox ]
+  environment.systemPackages = with pkgs; [ firefox ];
 }
--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ steam steam-run ]
+  environment.systemPackages = with pkgs; [ steam steam-run ];
 }
