876f2617f9f17ca60589b803e251331581d9b0f2
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2023-05-20 22:50:10 -0500

    Update g560 config

--- hosts/g560/configuration.nix
+++ hosts/g560/configuration.nix
@@ -13,7 +13,7 @@
 
     networking.hostName = "nixos-g560";
 
-    system.autoUpgrade.flake = "github:tjcardinal/nix-config#g560";
+    system.autoUpgrade.flake = "github:tjcardinal/nix-config#laptop-g560";
 
     system.stateVersion = "22.11";
 }
