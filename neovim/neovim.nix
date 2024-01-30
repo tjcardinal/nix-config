f336e5362ce62e60d0b5dbc7e3e065ea9d12b9c1
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2024-01-29 21:54:13 -0600

    Run nix fmt

--- neovim/neovim.nix
+++ neovim/neovim.nix
@@ -1,64 +1,64 @@
 pkgs:
-  pkgs.neovim.override {
-    configure = {
-      packages.myVimPackages = with pkgs.vimPlugins; {
-        start = [
-          nvim-treesitter.withAllGrammars
-          # nvim-cmp
-          which-key-nvim
-          plenary-nvim
-          telescope-nvim
-          nvim-lspconfig
-          onedark-nvim
-          indent-blankline-nvim
-        ];
-      };
+pkgs.neovim.override {
+  configure = {
+    packages.myVimPackages = with pkgs.vimPlugins; {
+      start = [
+        nvim-treesitter.withAllGrammars
+        # nvim-cmp
+        which-key-nvim
+        plenary-nvim
+        telescope-nvim
+        nvim-lspconfig
+        onedark-nvim
+        indent-blankline-nvim
+      ];
+    };
 
-      customRC = ''
-                " Display changes
-                set number
-                set relativenumber
+    customRC = ''
+              " Display changes
+              set number
+              set relativenumber
 
-                nnoremap <leader>ff <cmd>Telescope find_files<cr>
+              nnoremap <leader>ff <cmd>Telescope find_files<cr>
 
-                set cursorline
+              set cursorline
 
-                " cursorcolumn doesn't work with vscode-neovim
-                if !exists('g:vscode')
-                  set cursorcolumn
-                endif
+              " cursorcolumn doesn't work with vscode-neovim
+              if !exists('g:vscode')
+                set cursorcolumn
+              endif
 
-                set linebreak
+              set linebreak
 
-                " Functional changes
-                set ignorecase
-                set smartcase
+              " Functional changes
+              set ignorecase
+              set smartcase
 
-                set splitbelow
-                set splitright
+              set splitbelow
+              set splitright
 
-                set scrolloff=5
+              set scrolloff=5
 
-                inoremap jk <ESC>
-                nnoremap j gj
-                nnoremap k gk
+              inoremap jk <ESC>
+              nnoremap j gj
+              nnoremap k gk
 
-        lua <<EOF
-                require 'ibl'.setup()
+      lua <<EOF
+              require 'ibl'.setup()
 
                 
-                require 'nvim-treesitter.configs'.setup {
-                        highlight = {
-                                enable = true
-                        },
-                        indent = {
-                                enable = true
-                        },
-                }
-                require 'which-key'.setup {}
-                require'lspconfig'.rust_analyzer.setup{}
+              require 'nvim-treesitter.configs'.setup {
+                      highlight = {
+                              enable = true
+                      },
+                      indent = {
+                              enable = true
+                      },
+              }
+              require 'which-key'.setup {}
+              require'lspconfig'.rust_analyzer.setup{}
 
-        EOF
-      '';
-    };
-  }
+      EOF
+    '';
+  };
+}
