24f8719413fb10d811653c3cf7f24f5d844a7734
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2024-04-28 21:26:29 -0500

    Fix suspend lockup on y500 laptop

--- nixosConfigs/hosts/laptop-y500/laptop-y500.nix
+++ nixosConfigs/hosts/laptop-y500/laptop-y500.nix
@@ -16,10 +16,21 @@
 
   hardware = {
     opengl.enable = true;
-    nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    # Need to remove /lib/systemd/system-sleep/nvidia to prevent nvidia force resuming on suspend
+    nvidia.package = (config.boot.kernelPackages.nvidiaPackages.legacy_470.overrideAttrs (prevAttrs: {
+      postInstall = (toString prevAttrs.postInstall) + ''
+        rm $out/lib/systemd/system-sleep/nvidia
+      '';
+    }));
     nvidia.modesetting.enable = true;
   };
   nixpkgs.config.nvidia.acceptLicense = true;
+
+  # Disable nvidia services to force kernel driver callback to manage power management
+  # Using these can prevent waking from suspend when using nvidia 470
+  systemd.services.nvidia-hibernate.enable = false;
+  systemd.services.nvidia-resume.enable = false;
+  systemd.services.nvidia-suspend.enable = false;
 
   # Wayland needs to be disable since nvidia 470 doesn't work well with it
   services.xserver = {
