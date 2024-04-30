8a145472323979bed44ebd43d01a27bdc6b4c45e
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2024-04-29 22:06:21 -0500

    Fix suspend on y500 laptop again

--- nixosConfigs/hosts/laptop-y500/laptop-y500.nix
+++ nixosConfigs/hosts/laptop-y500/laptop-y500.nix
@@ -16,21 +16,18 @@
 
   hardware = {
     opengl.enable = true;
-    # Need to remove /lib/systemd/system-sleep/nvidia to prevent nvidia force resuming on suspend
-    nvidia.package = (config.boot.kernelPackages.nvidiaPackages.legacy_470.overrideAttrs (prevAttrs: {
-      postInstall = (toString prevAttrs.postInstall) + ''
-        rm $out/lib/systemd/system-sleep/nvidia
-      '';
-    }));
+    # Need to use 470.223.02 since 470.239.06 breaks suspend/resume
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "470.223.02";
+      sha256_64bit = "sha256-s2hi1TNsw+br6Ow6tPiFsYPaJY8d+x4FrkBrP2xNRPg=";
+      sha256_aarch64 = "sha256-CFkg2ARlGWqlFQKm8SlbwMH6eLidHKA/q5QGVOpPGuU=";
+      settingsSha256 = "sha256-r6DuIH/rnsCm/y51iRgPNi5/kz+EFMVABREdTjBneZ0=";
+      persistencedSha256 = "sha256-e71fpPBBv8S/aoeXxBXkzKy5bsMMbv8y024cSLc8DYc=";
+      patches = [];
+    };
     nvidia.modesetting.enable = true;
   };
   nixpkgs.config.nvidia.acceptLicense = true;
-
-  # Disable nvidia services to force kernel driver callback to manage power management
-  # Using these can prevent waking from suspend when using nvidia 470
-  systemd.services.nvidia-hibernate.enable = false;
-  systemd.services.nvidia-resume.enable = false;
-  systemd.services.nvidia-suspend.enable = false;
 
   # Wayland needs to be disable since nvidia 470 doesn't work well with it
   services.xserver = {
