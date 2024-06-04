4c829c4f0c07668f419adb870eb5675d74796ea1
Tyler Cardinal <83625450+tjcardinal@users.noreply.github.com>
2024-06-03 20:37:47 -0500

    Fix y500 graphical glitches

--- nixosConfigs/hosts/laptop-y500/laptop-y500.nix
+++ nixosConfigs/hosts/laptop-y500/laptop-y500.nix
@@ -20,20 +20,13 @@
       driSupport = true;
       driSupport32Bit = true;
     };
-
-    # Need to use 470.223.02 since 470.239.06 breaks suspend/resume
-    # nvidia.package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-    #   version = "470.223.02";
-    #   sha256_64bit = "sha256-s2hi1TNsw+br6Ow6tPiFsYPaJY8d+x4FrkBrP2xNRPg=";
-    #   sha256_aarch64 = "sha256-CFkg2ARlGWqlFQKm8SlbwMH6eLidHKA/q5QGVOpPGuU=";
-    #   settingsSha256 = "sha256-r6DuIH/rnsCm/y51iRgPNi5/kz+EFMVABREdTjBneZ0=";
-    #   persistencedSha256 = "sha256-e71fpPBBv8S/aoeXxBXkzKy5bsMMbv8y024cSLc8DYc=";
-    #   patches = [ ];
-    # };
     nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
     nvidia.modesetting.enable = true;
   };
   nixpkgs.config.nvidia.acceptLicense = true;
+  environment.sessionVariables = {
+    GSK_RENDERER = "gl";
+  };
 
   # Need to turn bluetooth off when suspending to prevent freezes
   powerManagement.powerUpCommands = "${pkgs.util-linux}/bin/rfkill unblock bluetooth";
